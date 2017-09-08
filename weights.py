4fa9f4360678cc8f73060f2a9e4b0f145f5e786d
josep.marin.llao <josep.marin.llao@scai.fraunhofer.de>
2017-09-08 13:18:30 +0200

    Weight.

--- weights.py
+++ weights.py
@@ -7,7 +7,6 @@ def get_approximate_maximum_d_value_from_three_by_two_contingency_table(three_by
                                                                         prediction_list_stats,
                                                                         log_of_factorial_of_prediction_list_stats,
                                                                         returnlog=False):
-
     n_pz = prediction_list_stats[1] - (three_by_two_contingency_table[1] + three_by_two_contingency_table[2])
     n_mz = prediction_list_stats[2] - (three_by_two_contingency_table[3] + three_by_two_contingency_table[4])
     n_zz = prediction_list_stats[3] - (three_by_two_contingency_table[5] + three_by_two_contingency_table[6])
@@ -123,8 +122,8 @@ def get_weights_above_hypothesis_score_for_a_three_by_two_table(
         for m_pp in range(n_pp, min(r_p, n_p)):
 
             # We still need to define one more value in the 3x2 matrix to determine all the rest n+- is bounded by the minumum of r- and n+
-            for m_mp in range(0,min(r_m, n_p)):
-    
+            for m_mp in range(0, min(r_m, n_p)):
+
                 # n+-
                 m_pm = r_p - m_pp
                 # n--
@@ -138,51 +137,47 @@ def get_weights_above_hypothesis_score_for_a_three_by_two_table(
                 m_pz = prediction_list_stats[1] - r_p
                 m_mz = prediction_list_stats[2] - r_m
                 m_zz = prediction_list_stats[3] - r_z
-    
+
                 # None of these values can be less than zero
                 if min(contingencyTableValues) < 0:
                     continue
 
                 threeByThreeContingencyTable = [m_pp, m_pm, m_pz, m_mp, m_mm, m_mz, m_zp, m_zm, m_zz]
-                logDValue = sum(log_of_factorial_of_prediction_list_stats) - sum(math.log(math.factorial(threeByThreeContingencyTable)))
-            # Only need to compute the score if the D-value is non-neglible
-            if logDValue > logepsDMax:
+                logDValue = sum(log_of_factorial_of_prediction_list_stats) - sum(
+                    math.log(math.factorial(threeByThreeContingencyTable)))
+                # Only need to compute the score if the D-value is non-neglible
+                if logDValue <= logepsDMax:
+                    continue
 
                 weight_contrib = math.exp(logDValue - logDMax)
                 weights[2] = weights[2] + weight_contrib
 
-        # Rather than just calculate the DValue by taking the exponential of logDvalue, we first subtract a constant.  This means when we take the
-        # exponential we get the DValue divided by a different constant, but this cancels out when we take ratios later when computing the p-value
-        if (m_pp + m_mm - (m_pm + m_mp) >= hypothesisScore)
-        {
-            weights[1] < - weights[1] + weight_contrib
-        }
-        }
-        }
-        }
-        }
+                # Rather than just calculate the DValue by taking the exponential of logDvalue, we first subtract a constant.  This means when we take the
+                # exponential we get the DValue divided by a different constant, but this cancels out when we take ratios later when computing the p-value
+                if m_pp + m_mm - (m_pm + m_mp) >= hypothesisScore:
+                    weights[1] = weights[1] + weight_contrib
 
         # The loop below does exactly the same thing as the one above; it covers the cases that are not done in the loop above. A lower bound for possible
         # values of n++ is 0 To avoid n_pp become negative or double counting when n_pp = 0, we add the following if statement
-        if (n_pp > 0) {
-        for (m_pp in (n_pp - 1):0) {
+        if (n_pp > 0):
+            for (m_pp in range(n_pp - 1, 0)):
 
         # Iterate over all values of m_mp
         for (m_mp in 0: min(r_m, n_p)) {
 
             # n+-
-            m_pm < - r_p - m_pp
+            m_pm = r_p - m_pp
         # n--
-        m_mm < - r_m - m_mp
+        m_mm = r_m - m_mp
         # n0+
-        m_zp < - n_p - (m_pp + m_mp)
+        m_zp = n_p - (m_pp + m_mp)
         # n0-
-        m_zm < - n_m - (m_pm + m_mm)
-        contingencyTableValues < - c(m_pp, m_pm, m_mp, m_mm, m_zp, m_zm)
+        m_zm = n_m - (m_pm + m_mm)
+        contingencyTableValues = c(m_pp, m_pm, m_mp, m_mm, m_zp, m_zm)
 
-        m_pz < - prediction_list_stats[1] - r_p
-        m_mz < - prediction_list_stats[2] - r_m
-        m_zz < - prediction_list_stats[3] - r_z
+        m_pz = prediction_list_stats[1] - r_p
+        m_mz = prediction_list_stats[2] - r_m
+        m_zz = prediction_list_stats[3] - r_z
 
         # None of these values can be less than zero
         if (min(contingencyTableValues) >= 0)

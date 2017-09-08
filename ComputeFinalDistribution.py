4c75d21473c20050177da663660729ca395f0451
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2017-09-08 11:00:44 +0200

    update in compute final distirbution

--- ComputeFinalDistribution.py
+++ ComputeFinalDistribution.py
@@ -1,33 +1,35 @@
+# -*- coding: utf-8 -*-
+
 import numpy
 
-def computeFinalDistribution(resultsMatrix):
+def compute_final_distribution(result_matrix):
     """ Computes a final reference distribution of the score used to compute the final p-value. 
     
-    :param numpy.array resultsMatrix: a numpy matrix
+    :param numpy.array result_matrix: a numpy matrix
     :rtype numpy.array
     :return distributionMatrix
     """
     
-    if resultsMatrix.size == 2: 
-        resultsMatrix = resultsMatrix.transpose()
+    if result_matrix.size == 2: 
+        result_matrix = result_matrix.transpose()
         
-    maxScore = max(resultsMatrix[:,0])
-    minScore = min(resultsMatrix[:,0])
+    maxScore = max(result_matrix[:,0])
+    minScore = min(result_matrix[:,0])
     
     #Pre-allocate the size of storage array
-    distributionMatrix = zeros(shape=((maxScore - minScore + 1),2))
-    numRows = resultsMatrix.shape[0]
+    distribution_matrix = zeros(shape=((maxScore - minScore + 1),2))
+    numRows = result_matrix.shape[0]
     counter = 0
     for score in range(minScore, maxScore+1):
         probability = 0
         for i in range(numRows):
-            if score == resultsMatrix[i,0]:
-                probability = probability + resultsMatrix[i,1]
+            if score == result_matrix[i,0]:
+                probability = probability + result_matrix[i,1]
         if probability > 0:
-            distributionMatrix[counter, ] = [score, probability]
+            distribution_matrix[counter, ] = [score, probability]
             counter += 1
     
     #Remove the rows that were not populated in the for loops above
-    distributionMatrix = distributionMatrix[0:counter,:]
+    distribution_matrix = distribution_matrix[0:counter,:]
     
-    return distributionMatrix
+    return distribution_matrix
